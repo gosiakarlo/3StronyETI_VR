8620e7bafba9c84c55372841ddc30e26a7273e38
Natalia Cyrklaff <natkapietruszki2000@gmail.com>
2024-01-10 19:42:22 +0100

    Added opening locker

--- Assets/Scripts/PuzzleItems/LockerItem.cs
+++ Assets/Scripts/PuzzleItems/LockerItem.cs
@@ -1,18 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class LockerItem : PuzzleItem
 {
-    // Start is called before the first frame update
-    void Start()
+    public XRSimpleInteractable simpleInteractable; // Reference to your XR Simple Interactable
+    public GameObject lockerClosed; // Reference to your closed locker GameObject
+    public GameObject lockerOpened; // Reference to your opened locker GameObject
+    public GameObject zipFolder;
+
+    private void OnEnable()
     {
-        
+        // Subscribe to the event
+        simpleInteractable.activated.AddListener(HandleActivated);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnDisable()
     {
-        
+        // Unsubscribe from the event
+        simpleInteractable.activated.RemoveListener(HandleActivated);
+    }
+
+    private void HandleActivated(ActivateEventArgs arg)
+    {
+        OpenTheLocker();
+    }
+
+    public void OpenTheLocker()
+    {
+        lockerOpened.SetActive(!lockerOpened.activeSelf);
+        lockerClosed.SetActive(!lockerClosed.activeSelf);   //!lockerClosed.activeSelf
+        zipFolder.SetActive(true);
     }
 }
